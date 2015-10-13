3efd686a709d1f0fd6b54e5911ce28acdf7ef88c
Catalina <catafaunep@gmail.com>
2015-10-12 22:40:35 -0300

    primeras vistas creadas

--- mysite/remedios/admin.py
+++ mysite/remedios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Remedio, Presentacion
+
+admin.site.register(Remedio)
+admin.site.register(Presentacion)
+
 # Register your models here.
--- mysite/remedios/views.py
+++ mysite/remedios/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+    latest_remedio_list = Remedio.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('remedios/index.html')
+    context = RequestContext(request, {
+        'latest_remedio_list': latest_remedio_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, remedio_id):
+    return HttpResponse("You're looking at question %s." % remedio_id)
+
+def results(request, remedio_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % remedio_id)
+
+def vote(request, remedio_id):
+    return HttpResponse("You're voting on question %s." % remedio_id)
+
+
 
 # Create your views here.
