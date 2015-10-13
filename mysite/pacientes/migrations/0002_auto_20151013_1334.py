8ab07387974436013cd6ca8f491f396194f0ca1e
Catalina <catafaunep@gmail.com>
2015-10-13 10:35:17 -0300

    Diagnostico y Situacion agregados

---
+++ mysite/pacientes/migrations/0002_auto_20151013_1334.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pacientes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Diagnostico',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(unique=True, max_length=255)),
+                ('descripcion', models.CharField(unique=True, max_length=255)),
+                ('paciente', models.ForeignKey(to='pacientes.Paciente')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Situacion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(unique=True, max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='diagnostico',
+            name='situacion',
+            field=models.ForeignKey(to='pacientes.Situacion'),
+        ),
+    ]
--- mysite/pacientes/models.py
+++ mysite/pacientes/models.py
@@ -19,3 +19,12 @@ class Paciente(models.Model):
 
 	def __str__(self):
 		return self.rut+" "+self.apellido_paterno+" "+self.apellido_materno+", "+self.nombre+" - "+self.prevision.nombre
+
+class Situacion(models.Model):
+	nombre=models.CharField(max_length=255,unique=True)
+
+class Diagnostico(models.Model):
+	paciente=models.ForeignKey(Paciente,null=False)
+	nombre=models.CharField(max_length=255,unique=True)
+	descripcion=models.CharField(max_length=255,unique=True)
+	situacion=models.ForeignKey(Situacion,null=False)
